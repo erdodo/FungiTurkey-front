15137c9dfcc079f4e9b23c3a12ce5d91a313f5ce
erdodo <erdoganyesil3@gmail.com>
2022-08-23 20:56:39 +0300

    hadi bakalım

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 axios.defaults.headers.common["token"] = 0; //localStorage.getItem("token");
 axios.defaults.headers.common["Content-Type"] = "application/json";
-
+axios.defaults.headers.common["Host"] = window.location.host;
 axios.defaults.baseURL = "http://erdodo.serveftp.com/Simple/api/";
 
 import ElementPlus from "element-plus";
