7e97086ca1cdd677820c4c3d2834ad2286d85b80
erdodo <erdoganyesil3@gmail.com>
2022-08-23 20:44:17 +0300

    neden

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.headers.common["token"] = localStorage.getItem("token");
+axios.defaults.headers.common["token"] = 0; //localStorage.getItem("token");
 axios.defaults.headers.common["Content-Type"] = "application/json";
 
 axios.defaults.baseURL = "http://erdodo.serveftp.com/Simple/api/";
